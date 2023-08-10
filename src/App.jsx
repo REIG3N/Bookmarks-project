284fdb3548261596f8f828fd536dc2e70a097c02
ryanb <ryan.bardat.123@gmail.com>
2023-08-10 18:27:46 +0200

    fixing urls

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ export default function App() {
     <>
       <Navbar />
         <Routes>
+          <Route path='/' element={<MyBooks />} />
           <Route path='/mybooks' element={<MyBooks />} />
           <Route path='/mydiary' element={<MyDiary />} />
         </Routes>
